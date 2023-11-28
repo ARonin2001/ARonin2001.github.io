fe979860b177c57254998aef98fc2e6726887dc3
ARonin2001 <aronin.2001@gmail.com>
2023-11-28 20:20:34 +0400

    CSS grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import './ResetStyles.module.scss';
+import style from './App.module.scss';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div className="App">Hello Framework Team</div>;
+  return (
+    <div className={style.main}>
+      <div className={style.container}>
+        <div className={style.main__header}>
+          <Header />
+        </div>
+
+        <div className={style.main__filter}>
+          <div className={style.filter}>Filter</div>
+        </div>
+
+        <div className={style.main__gallery}>
+          <div className={style.gallery}>Gallery</div>
+        </div>
+
+        <div className={style.main__pagination}>
+          <div className={style.pagination}>Pagination</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
